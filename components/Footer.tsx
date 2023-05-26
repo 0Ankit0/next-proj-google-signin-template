2a09612ee3d90d0794e5bbd562640cc34d0c8c84
ankit <ankit@DESKTOP>
2023-05-26 18:10:59 +0545

    sign-in with google

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+import packageJSON from "../package.json"
+
+export default function Footer() {
+  return (
+    <footer >
+      <hr />
+      <h2>This is footer</h2>
+    </footer>
+  )
+}
---
+++ components/Layout.tsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import type { ReactNode } from "react"
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
